d36cfa418c2ba5b8db9090a67538acbff0be29a5
Robert Adams <misterblue@misterblue.com>
2026-09-06 09:47:54 -0700

    Update MBLogger to base off LogLevel and add LogSelections from config files.

--- src/Logging/MBLogger.cs
+++ src/Logging/MBLogger.cs
@@ -31,58 +31,61 @@ namespace org.herbal3d.mblue.Logging {
             _log = pLogger ?? throw new ArgumentNullException(nameof(pLogger));
             _options = pOptions ?? throw new ArgumentNullException(nameof(pOptions));
 
-            // If "LogLevel" is not set, expect a list of setting in "LogSelections" to
-            //    determine the log level flags.
-            // That is, if "LogLevel" is set, it overrides "LogSelections".
-            if (String.IsNullOrWhiteSpace(_options.Value.LogLevel)) {
-                // Set the log level flags based on the configuration
+            // "LogLevel" sets the general log level for the application.
+            // "LogSelections" are added to the log level if "LogLevel" is "debug".
+            // If "LogLevel" is set, use it to determine the log level flags
+            MBLogLevel allBadThings = MBLogLevel.Error
+                                        | MBLogLevel.Critical
+                                        | MBLogLevel.DBADERROR;
+            switch (_options.Value.LogLevel.Trim().ToLowerInvariant()) {
+                case "none":
+                    _logLevelFlags = MBLogLevel.None;
+                    _log.LogInformation("Log level set to 'None' from configuration.");
+                    break;
+                case "trace":
+                    _logLevelFlags = allBadThings | MBLogLevel.Trace;
+                    _log.LogInformation("Log level set to 'Trace' from configuration.");
+                    break;
+                case "debug":
+                    _logLevelFlags = allBadThings | MBLogLevel.Debug;
+                    _log.LogInformation("Log level set to 'Debug' from configuration.");
+                    break;
+                case "information":
+                    _logLevelFlags = allBadThings | MBLogLevel.Information;
+                    _log.LogInformation("Log level set to 'Information' from configuration.");
+                    break;
+                case "warning":
+                    _logLevelFlags = allBadThings | MBLogLevel.Warning;
+                    _log.LogInformation("Log level set to 'Warning' from configuration.");
+                    break;
+                case "error":
+                    _logLevelFlags = allBadThings | MBLogLevel.Error;
+                    _log.LogInformation("Log level set to 'Error' from configuration.");
+                    break;
+                case "critical":
+                    _logLevelFlags = allBadThings | MBLogLevel.Critical;
+                    _log.LogInformation("Log level set to 'Critical' from configuration.");
+                    break;
+                default:
+                    // Log a warning if the user provided an invalid log level
+                    _log.LogWarning($"Invalid log level '{_options.Value.LogLevel}' in configuration. Defaulting to 'Debug'.");
+                    _logLevelFlags = allBadThings | MBLogLevel.Debug;
+                    break;
+            }
+            if ((_logLevelFlags & MBLogLevel.Debug) != 0) {
                 // "LogSelections" is a comma-separated list of log levels, e.g., "Critical,Error,Warning,Information"
                 string[] userSelections = _options.Value.LogSelections.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
                 MBLogLevel flags = MBLogLevel.None;
                 foreach (string selection in userSelections) {
                     if (Enum.TryParse<MBLogLevel>(selection.Trim(), true, out MBLogLevel parsedLevel)) {
+                        _log.LogInformation($"Adding log level '{selection}' from configuration.");
                         flags |= parsedLevel;
                     } else {
                         // Log a warning if the user provided an invalid log level
                         _log.LogWarning($"Invalid log level '{selection}' in configuration. Ignoring.");
                     }
                 }
-                _logLevelFlags = flags;
-            } else {
-                // If "LogLevel" is set, use it to determine the log level flags
-                MBLogLevel allBadThings = MBLogLevel.Information
-                                            | MBLogLevel.Warning
-                                            | MBLogLevel.Error
-                                            | MBLogLevel.Critical
-                                            | MBLogLevel.DBADERROR;
-                switch (_options.Value.LogLevel.Trim().ToLowerInvariant()) {
-                    case "none":
-                        _logLevelFlags = MBLogLevel.None;
-                        break;
-                    case "trace":
-                        _logLevelFlags = allBadThings | MBLogLevel.Trace;
-                        break;
-                    case "debug":
-                        _logLevelFlags = allBadThings | MBLogLevel.Debug;
-                        break;
-                    case "information":
-                        _logLevelFlags = allBadThings | MBLogLevel.Information;
-                        break;
-                    case "warning":
-                        _logLevelFlags = allBadThings | MBLogLevel.Warning;
-                        break;
-                    case "error":
-                        _logLevelFlags = allBadThings | MBLogLevel.Error;
-                        break;
-                    case "critical":
-                        _logLevelFlags = allBadThings | MBLogLevel.Critical;
-                        break;
-                    default:
-                        // Log a warning if the user provided an invalid log level
-                        _log.LogWarning($"Invalid log level '{_options.Value.LogLevel}' in configuration. Defaulting to 'Debug'.");
-                        _logLevelFlags = allBadThings | MBLogLevel.Debug;
-                        break;
-                }
+                _logLevelFlags |= flags;
             }
         }
 
@@ -105,40 +108,8 @@ namespace org.herbal3d.mblue.Logging {
 
         // Log function that is used everywhere
         public void Log(MBLogLevel pLevel, string pMessage, params object[] pArgs) {
-            switch (pLevel) {
-                case MBLogLevel.Trace:
-                    _log.Log(LogLevel.Trace, pMessage, pArgs);
-                    break;
-                case MBLogLevel.Debug:
-                    _log.Log(LogLevel.Debug, pMessage, pArgs);
-                    break;
-                case MBLogLevel.Information:
-                    _log.Log(LogLevel.Information, pMessage, pArgs);
-                    break;
-                case MBLogLevel.Warning:
-                    _log.Log(LogLevel.Warning, pMessage, pArgs);
-                    break;
-                case MBLogLevel.Error:
-                    _log.Log(LogLevel.Error, pMessage, pArgs);
-                    break;
-                case MBLogLevel.Critical:
-                    _log.Log(LogLevel.Critical, pMessage, pArgs);
-                    break;
-                case MBLogLevel.None:
-                    // Do nothing
-                    break;
-                case MBLogLevel.DBADERROR:
-                    _log.Log(LogLevel.Error, pMessage, pArgs);
-                    break;
-                default:
-                    // Might be one of the detailed debugging flags
-                    if (((ulong)_logLevelFlags & (ulong)pLevel) != 0) {
-                        _log.Log(LogLevel.Information, pMessage, pArgs);
-                    } else {
-                        // Unknown level, log as Information
-                        _log.Log(LogLevel.Information, pMessage, pArgs);
-                    }
-                    break;
+            if (((ulong)_logLevelFlags & (ulong)pLevel) != 0) {
+                _log.Log(LogLevel.Information, pMessage, pArgs);
             }
         }
 
